a78f9d867b734bd633632010bd28911fa14fc1b6
Adam Driscoll <adam.r.driscoll@gmail.com>
2023-04-25 15:06:52 -0500

    Icons.

--- MaterialDesign/components/component.jsx
+++ MaterialDesign/components/component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { withComponentFeatures } from 'universal-dashboard'
 import * as MDIcons from 'react-icons/md';
-
+import { IconContext } from 'react-icons/lib';
 const UDComponent = props => {
-    return React.createElement(MDIcons[props.icon], props);
+    return <IconContext.Provider value={{ ...props }}>
+        {React.createElement(MDIcons[props.icon])}
+    </IconContext.Provider>
 }
 
 export default withComponentFeatures(UDComponent)
